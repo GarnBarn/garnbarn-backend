ebdef0211c177199860fdedffe955a88d50916b3
Poomtum <poomtum2544@hotmail.com>
2021-10-14 22:17:53 +0700

    :hammer: add route for api

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('config.urls')),
+    path('api/', include('garnbarn_api.urls')),
 ]
