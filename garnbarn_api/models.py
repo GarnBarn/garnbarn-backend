1e936cf796320e4b89e8a03d73baa3fb1b3dd769
SoSorryTT <markpanya@gmail.com>
2021-10-19 21:37:13 +0700

    Create tag model

--- garnbarn_api/models.py
+++ garnbarn_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models import deletion
 
-# Create your models here.
+
+class Tag(models.Model):
+    """Class that contain tags or subject of the users."""
+
+    tag_name = models.CharField(max_length=200)
+    tag_id = models.CharField(max_length=200)
+
+    def get_tag_id(self):
+        """Return id of the tag."""
+        return self.tag_id
+
+    def delete_tag(self):
+        """Delete the tag."""
+        pass
+
+    def __str__(self):
+        """Return name of the tag."""
+        return self.tag_name
