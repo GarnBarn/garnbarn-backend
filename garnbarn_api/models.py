85666b0c4fbed9868d5e92cd683ff60d44835fba
abounk <a_bunk911@hotmail.com>
2021-10-19 22:56:35 +0700

    Initialize assignment's model

--- garnbarn_api/models.py
+++ garnbarn_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Assignment(models.Model):
+
+    assignment_name = models.CharField(max_length=200)
+    due_date = models.DateTimeField('due date')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.assignment_name
+
+    def get_assignment(self):
+        pass
+
+    def remove_assignment(self, pk):
+        pass
