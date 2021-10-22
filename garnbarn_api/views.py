a572d635ca06664e0dcd714688cc23b986b63693
abounk <a_bunk911@hotmail.com>
2021-10-22 20:14:24 +0700

    There's no bugs, modify create method to return bad request status when there's not enough data

--- garnbarn_api/views.py
+++ garnbarn_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from rest_framework import serializers, viewsets
+from rest_framework import serializers, viewsets, status
 from .serializer import AssignmentSerializer
 
 from .models import Assignment, Tag
@@ -12,22 +12,22 @@ class AssignmentViewset(viewsets.ModelViewSet):
     queryset = Assignment.objects.get_queryset().order_by('id')
 
     def create(self, request, *args, **kwargs):
-        assignment_data = request.data
-        new_assignment = Assignment.objects.create(assignment_name=assignment_data["assignment_name"],
-                                                   due_date=assignment_data["due_date"],
-                                                   detail=assignment_data["detail"],
-                                                   tag=Tag.objects.get(
-                                                       id=assignment_data["tag"])
-                                                   )
-        new_assignment.save()
-        serializer = AssignmentSerializer(new_assignment)
-        return Response(serializer.data)
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            Assignment.objects.create(**serializer.validated_data)
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+        return Response({
+            'status': 'Bad Request',
+            'message': 'Assignment could not be created with received data'
+        }, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, *args, **kwargs):
         assignment = self.get_object()
+        assignment_name = assignment.assignment_name
         assignment.delete()
 
-        return Response({"message": "Item has been deleted"})
+        return Response({"message": assignment_name + " has been deleted"})
 
     def update(self, request, *args, **kwargs):
         assignment_object = self.get_object()
