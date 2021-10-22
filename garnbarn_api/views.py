36a4982ac4af047d48f75d681cbe46b8810c567a
abounk <a_bunk911@hotmail.com>
2021-10-22 15:51:50 +0700

    Add create method to AssignmentViewset (Bugs: request.data is empty)

--- garnbarn_api/views.py
+++ garnbarn_api/views.py
@@ -11,14 +11,19 @@ class AssignmentViewset(viewsets.ModelViewSet):
     serializer_class = AssignmentSerializer
 
     def get_queryset(self):
-        assignment = Assignment.objects.all()
+        assignment = Assignment.objects.get_queryset().order_by('id')
         return assignment
 
-    def retrieve(self, request, *args, **kwargs):
-        """This method filter the data inside ModelViewset."""
-        params = kwargs
-        assignment = Assignment.objects.filter(assignment_name=params['pk'])
-        serializer = AssignmentSerializer(assignment)
+    def create(self, request, *args, **kwargs):
+        """Override create action."""
+
+        assignment_data = request.data
+        print(assignment_data)
+        new_assignment = Assignment.objects.create(assignment_name=assignment_data["assignment_name"],
+                                                   due_date=assignment_data["due_date"],
+                                                   detail=assignment_data["detail"])
+        new_assignment.save()
+        serializer = AssignmentSerializer(new_assignment)
         return Response(serializer.data)
 
 
