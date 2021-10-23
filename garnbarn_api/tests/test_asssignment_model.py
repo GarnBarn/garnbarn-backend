6c07cb8aac71f88cbb87b09c767e957ddffd8ec1
SoSorryTT <markpanya@gmail.com>
2021-10-23 23:04:14 +0700

    Create assignment model test

--- garnbarn_api/tests/test_asssignment_model.py
+++ garnbarn_api/tests/test_asssignment_model.py
@@ -2,3 +2,6 @@ from django.test import TestCase
 from garnbarn_api.models import Assignment
 
 
+class AssignmentModelTests(TestCase):
+    def setUp(self):
+        self.assignment = Assignment(assignment_name='Test case')
