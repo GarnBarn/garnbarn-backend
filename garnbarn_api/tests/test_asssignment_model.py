b47c8e1234964d217e476202a3f29711acc9d7c9
abounk <a_bunk911@hotmail.com>
2021-10-23 19:16:14 +0700

    add setup for assignment model test

--- garnbarn_api/tests/test_asssignment_model.py
+++ garnbarn_api/tests/test_asssignment_model.py
@@ -2,3 +2,10 @@ from django.test import TestCase
 from garnbarn_api.models import Assignment
 
 
+class AssignmentModelTests(TestCase):
+    def setUp(self):
+        self.assignment = Assignment.objects.create(
+            assignment_name="test_name",
+            due_date=6543210000,
+            detail="test_detail"
+        )
