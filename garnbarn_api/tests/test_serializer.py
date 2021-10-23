bc56fc937e2929ff27c5fb5810f1db2c03ad863c
SoSorryTT <markpanya@gmail.com>
2021-10-23 23:50:06 +0700

    Create is published test

--- garnbarn_api/tests/test_serializer.py
+++ garnbarn_api/tests/test_serializer.py
@@ -18,14 +18,14 @@ class SerializerTests(TestCase):
             'id': 1,
             'tag': None,
             'assignment_name': 'test_with_serializer',
-            'due_date': '2021-10-27T19:09:03+07:00',
+            'due_date': 1635361743,
             'timestamp': '2021-10-27T19:09:03+07:00',
             'detail': 'test_with_serializer'
         }
 
         self.assignment_object = Assignment.objects.create(
             assignment_name='test_with_serializer',
-            due_date='2021-10-27T19:09:03+07:00',
+            due_date='2021-10-27T19:09',
             detail='test_with_serializer'
             )
         self.serializer = AssignmentSerializer(instance=self.assignment_object)
@@ -48,3 +48,7 @@ class SerializerTests(TestCase):
         self.assertEqual(data['assignment_name'], self.assignment_attributes['assignment_name'])
         self.assertEqual(data['due_date'], self.assignment_attributes['due_date'])
         self.assertEqual(data['detail'], self.assignment_attributes['detail'])
+
+    def test_is_published(self):
+        data = self.serializer.data
+        self.assertEqual(True, data.serializer.is_published())
