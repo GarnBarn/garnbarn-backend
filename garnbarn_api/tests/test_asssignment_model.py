f72f9c9191c1c6b58d36f76c0c469409b5a50d00
abounk <a_bunk911@hotmail.com>
2021-10-23 19:22:43 +0700

    edit due_date in assignment model test setup

--- garnbarn_api/tests/test_asssignment_model.py
+++ garnbarn_api/tests/test_asssignment_model.py
@@ -6,6 +6,6 @@ class AssignmentModelTests(TestCase):
     def setUp(self):
         self.assignment = Assignment.objects.create(
             assignment_name="test_name",
-            due_date=6543210000,
+            due_date=1635336554,
             detail="test_detail"
         )
