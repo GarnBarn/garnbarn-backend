9ae12ab29cd53d340377edc35dea9546f9a2afde
abounk <a_bunk911@hotmail.com>
2021-10-23 16:18:33 +0700

    set datetime format to int

--- config/settings.py
+++ config/settings.py
@@ -149,5 +149,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%s'
 }
