467b7c83958074235e68ebfd2d24c640cc4a184a
SoSorryTT <markpanya@gmail.com>
2021-10-24 00:16:37 +0700

    Fix test case for is published

--- garnbarn_api/tests/test_serializer.py
+++ garnbarn_api/tests/test_serializer.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 from django.test import TestCase
 from garnbarn_api.serializer import AssignmentSerializer
 from garnbarn_api.models import Assignment
@@ -6,29 +6,37 @@ from garnbarn_api.models import Assignment
 
 class SerializerTests(TestCase):
     def setUp(self):
+        due_date1 = datetime.fromtimestamp(1635361743)
+        due_date2 = datetime.fromtimestamp(1603825743)
         self.assignment_attributes = {
             'id': 1,
             'tag': None,
             'assignment_name': 'test_with_serializer',
-            'due_date': '2021-10-27T19:09:03+07:00',
+            'due_date': due_date1.timestamp(),
             'timestamp': '2021-10-27T19:09:03+07:00',
             'detail': 'test_with_serializer'
         }
-        self.serializer_data = {
-            'id': 1,
-            'tag': None,
-            'assignment_name': 'test_with_serializer',
-            'due_date': 1635361743,
-            'timestamp': '2021-10-27T19:09:03+07:00',
-            'detail': 'test_with_serializer'
-        }
+        # self.serializer_data = {
+        #     'id': 1,
+        #     'tag': None,
+        #     'assignment_name': 'test_with_serializer',
+        #     'due_date': '2021-10-27T19:09:03+07:00',
+        #     'timestamp': '2021-10-27T19:09:03+07:00',
+        #     'detail': 'test_with_serializer'
+        # }
 
         self.assignment_object = Assignment.objects.create(
             assignment_name='test_with_serializer',
-            due_date='2021-10-27T19:09',
+            due_date = due_date1,
+            detail='test_with_serializer'
+            )
+        self.assignment_object2 = Assignment.objects.create(
+            assignment_name='test_with_serializer',
+            due_date = due_date2,
             detail='test_with_serializer'
             )
         self.serializer = AssignmentSerializer(instance=self.assignment_object)
+        self.serializer2 = AssignmentSerializer(instance=self.assignment_object2)
 
     def test_serializer_contain_expected_field(self):
         data = self.serializer.data
@@ -49,6 +57,10 @@ class SerializerTests(TestCase):
         self.assertEqual(data['due_date'], self.assignment_attributes['due_date'])
         self.assertEqual(data['detail'], self.assignment_attributes['detail'])
 
-    def test_is_published(self):
+    def test_is_published_return_True(self):
         data = self.serializer.data
         self.assertEqual(True, data.serializer.is_published())
+
+    def test_is_published_return_False(self):
+        data = self.serializer2.data
+        self.assertEqual(False, data.serializer.is_published())
