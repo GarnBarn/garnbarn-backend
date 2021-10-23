840723269e030cfd1351330a9783889e768db80f
SoSorryTT <markpanya@gmail.com>
2021-10-23 18:51:16 +0700

    Create test to test Tag object

---
+++ garnbarn_api/tests/test_asssignment_model.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+from garnbarn_api.models import Assignment
+
+
---
+++ garnbarn_api/tests/test_tag_model.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from garnbarn_api.models import Tag
+
+
+class TagModelTests(TestCase):
+    def setUp(self):
+        self.tag = Tag(tag_name='tag1')
+
+    def test_get_tag_name(self):
+        """Test that tag object will return tag name in string."""
+        self.assertEqual('tag1', str(self.tag))
