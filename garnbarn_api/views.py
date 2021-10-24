ebb87b41935aa66ab8b870844ae1ba22a3e98698
abounk <a_bunk911@hotmail.com>
2021-10-25 01:19:29 +0700

    remove unused import

--- garnbarn_api/views.py
+++ garnbarn_api/views.py
@@ -1,7 +1,5 @@
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
-from rest_framework import serializers, viewsets, status
+from rest_framework import viewsets, status
 from .serializer import AssignmentSerializer
 
 from .models import Assignment, Tag
