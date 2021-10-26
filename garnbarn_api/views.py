23219295a1badf53126572e95359a4a06faa4a8f
Siratee K <sirateek@gmail.com>
2021-10-26 11:29:11 +0700

    Make Create Assignment API accept the string that must be able to be converted to an integer and improve it stability

--- garnbarn_api/views.py
+++ garnbarn_api/views.py
@@ -22,26 +22,33 @@ class AssignmentViewset(viewsets.ModelViewSet):
         serializer = AssignmentSerializer(data=data)
         message = 'Assignment could not be created with received data'
 
-        if serializer.is_valid(raise_exception=True):
-            assignment_object = Assignment(**serializer.validated_data)
+        if not serializer.is_valid(raise_exception=True):
+            # Response 400 if the reuqest body is invalid
+            return Response({
+                'message': message
+            }, status=status.HTTP_400_BAD_REQUEST)
 
-            # add tag to assignment by giving the tag's id
+        assignment_object = Assignment(**serializer.validated_data)
+
+        # add tag to assignment by giving the tag's id
+        tag_id_from_request = request.data.get("tagId")
+        if tag_id_from_request is not None:
             try:
-                tag_id = int(request.data["tagId"])
-                assignment_object.tag = Tag.objects.get(id=tag_id)
+                tag_id_from_request = int(tag_id_from_request)
+                assignment_object.tag = Tag.objects.get(id=tag_id_from_request)
             except Tag.DoesNotExist:
                 return Response({
                     'message': "Tag's ID not found"
                 }, status=status.HTTP_400_BAD_REQUEST)
+            except ValueError:
+                return Response({
+                    'message': "tagId must be able to be converted to an integer"
+                }, status=status.HTTP_400_BAD_REQUEST)
 
-            assignment_object.save()
+        assignment_object.save()
 
-            assignment_serializer = AssignmentSerializer(assignment_object)
-            return Response(assignment_serializer.data, status=status.HTTP_200_OK)
-
-        return Response({
-            'message': message
-        }, status=status.HTTP_400_BAD_REQUEST)
+        assignment_serializer = AssignmentSerializer(assignment_object)
+        return Response(assignment_serializer.data, status=status.HTTP_200_OK)
 
     def destroy(self, request, *args, **kwargs):
         """ Remove assignment with specified id.
