39d7455d05e6eadb20228be331046303a9df10c3
SoSorryTT <markpanya@gmail.com>
2021-10-28 18:07:16 +0700

    Create test to test user model and write docstring

--- garnbarn_api/tests/test_social_object.py
+++ garnbarn_api/tests/test_social_object.py
@@ -2,10 +2,10 @@ from django.test import TestCase
 from garnbarn_api.models import SocialObject
 
 class TestModelSocialObject(TestCase):
-    """Create setup for SocialObject class."""
-
     def setUp(self):
+        """Create setup for SocialObject class."""
         self.social = SocialObject(social_id='U1efbc797c7174dd636c047f5ca8eba42')
 
     def test_social_id(self):
+        """Test social id of the object."""
         self.assertEqual('U1efbc797c7174dd636c047f5ca8eba42', self.social.social_id)
