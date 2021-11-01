a19a1dfc13308adf1bdc9f3e675c561b6f603c2b
SoSorryTT <markpanya@gmail.com>
2021-11-01 17:30:41 +0700

    Create test case for tag serializer

---
+++ garnbarn_api/tests/test_tag_serializer.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from garnbarn_api.models import Tag
+from garnbarn_api.serializer import CreateTagApiSerializer
+
+
+class TagAPITest(TestCase):
+    """Test case for Tag API."""
+
+    def setUp(self):
+        self.tag_attributes = {
+            'id': 1,
+            'name': "ISP",
+            'author': None,
+            'color': "#4285F4",
+            'reminderTime': [3600, 1800]
+        }
+
+        self.tag_object1 = Tag.objects.create(
+            name='ISP',
+            color='#4285F4',
+            author=None,
+            reminder_time=[3600, 1800]
+        )
+
+        self.serializer1 = CreateTagApiSerializer(
+            instance=self.tag_object1
+        )
+
+    def test_serializer_contain_expected_tag_field(self):
+        """Test that serializer have all the header."""
+        data = self.serializer1.data
+        self.assertEqual(set(data.keys()), set({
+            'id',
+            'name',
+            'color',
+            'author',
+            'reminderTime'
+        }))
+
+    def test_tag_field_content(self):
+        """Test that serializer have all the content in side the header."""
+        data = self.serializer1.data
+        self.assertEqual(data['id'], self.tag_attributes['id'])
+        self.assertEqual(data['name'], self.tag_attributes['name'])
+        self.assertEqual(data['color'], self.tag_attributes['color'])
+        self.assertEqual(data['author'], self.tag_attributes['author'])
+        self.assertEqual(data['reminderTime'], self.tag_attributes['reminderTime'])
+
