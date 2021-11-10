5bee8d6f4779f435b0ea8b26dbe9ee76c20e9e12
Siratee K <sirateek@gmail.com>
2021-11-11 02:21:49 +0700

    HotFix: Make fromPresent Parameter also return only the if the assignment is owned by request sender or in the assignment's tag accessible

--- garnbarn_api/views.py
+++ garnbarn_api/views.py
@@ -116,12 +116,14 @@ class AssignmentViewset(viewsets.ModelViewSet):
         user_data = self.request.user.uid
 
         if self.request.query_params.get('fromPresent') == "true":
-            assignment = Assignment.objects.exclude(
+            assignment = Assignment.objects.get_queryset().filter(
+                Q(author=user_data) | Q(tag__subscriber__icontains=user_data) | Q(tag__author=user_data))
+            assignment = assignment.exclude(
                 due_date__lt=date.today())
             assignment = assignment.exclude(due_date=None).order_by('due_date')
         else:
             assignment = Assignment.objects.get_queryset().filter(
-                Q(author=user_data) | Q(tag__subscriber__icontains=user_data)).order_by('id')
+                Q(author=user_data) | Q(tag__subscriber__icontains=user_data) | Q(tag__author=user_data)).order_by('id')
         return assignment
 
     def create(self, request, *args, **kwargs):
