3f3eea275628eca7af1ac5cc32e0651a6e1dac00
abounk <a_bunk911@hotmail.com>
2021-11-15 16:57:34 +0700

    call schedule.start() in ready() instead of scheduler.py

--- garnbarn_api/services/scheduler.py
+++ garnbarn_api/services/scheduler.py
@@ -1,13 +1,8 @@
-import logging
-
 from django.conf import settings
 
 from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
-from django.core.management.base import BaseCommand
 from django_apscheduler.jobstores import DjangoJobStore
 
 
 scheduler = BackgroundScheduler(timezone=settings.TIME_ZONE)
 scheduler.add_jobstore(DjangoJobStore(), "default")
-scheduler.start()
