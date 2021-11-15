faff2050cc00f5cbc72a362d67c171b49508a0e2
abounk <a_bunk911@hotmail.com>
2021-11-15 16:58:25 +0700

    remove unused code

--- garnbarn_api/models.py
+++ garnbarn_api/models.py
@@ -11,9 +11,6 @@ from apscheduler.triggers.date import DateTrigger
 
 import logging
 
-FORMAT = "%(asctime)-15s %(message)s"
-logging.basicConfig(format=FORMAT, level=logging.INFO,
-                    datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 
