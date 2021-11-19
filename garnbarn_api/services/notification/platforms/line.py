34087ce62d7547fa1f25f4b27d3cffb2207d5fc1
Siratee K <sirateek@gmail.com>
2021-11-19 21:46:25 +0700

    Make the notification check if the user has linked with the LINE Account before sending notification

--- garnbarn_api/services/notification/platforms/line.py
+++ garnbarn_api/services/notification/platforms/line.py
@@ -10,5 +10,10 @@ class Line(NotificationPlatform):
     messaging_api = LineMessagingApiHelper()
 
     def notification_handler(self, assignment_obj, user_obj):
+        """Send notification to user on LINE Platform."""
+        if not user_obj.line:
+            logger.warn(
+                f"User {user_obj.uid} doesn't linked any LINE Account. Skipping sending the notification for the assignment {assignment_obj.id}")
+            return
         self.messaging_api.send_assignment_notification(
             user_obj.line, assignment_obj)
